072e402ba96dc7d45b9ae4146f84bcefffcf0793
sanjay bhambhani <sanjay.bhambani@litmus7.com>
2023-02-23 21:38:13 +0530

    testing husky

--- src/widget/widget.tsx
+++ src/widget/widget.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import './tailwind.css';
+import './custom.css';
 
-function Widget() {
+const Widget = () => {
   const [count] = React.useState(0);
+  const clickHandler = () => {
+    console.log('Click Handler Called');
+  };
   return (
     <>
-      <p>
-                    You clicked  "sadasd"
+      <p className="app">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        You clicked a button!!!!!---
         {count}
       </p>
-      <button type="button">Click me sada</button>
+      <button type="button" onClick={clickHandler}>
+        Click me sada
+      </button>
     </>
   );
-}
+};
 
 export default Widget;
