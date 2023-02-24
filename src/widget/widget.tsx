03b0925b35c70101561ebb8b45dee92248a1b562
sanjay bhambhani <sanjay.bhambani@litmus7.com>
2023-02-24 09:59:57 +0530

    minified js

--- src/widget/widget.tsx
+++ src/widget/widget.tsx
@@ -4,19 +4,14 @@ import './custom.css';
 
 const Widget = () => {
   const [count] = React.useState(0);
-  const clickHandler = () => {
-    console.log('Click Handler Called');
-  };
   return (
     <>
       <p className="app">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        You clicked a button!!!!!---
+        You clicked a button!!!!!
         {count}
       </p>
-      <button type="button" onClick={clickHandler}>
-        Click me sada
-      </button>
+      <button type="button">Click me</button>
     </>
   );
 };
