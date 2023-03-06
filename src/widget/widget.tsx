ba9f3445286286077102a9a1cc2c71a0792e5ef7
sanjay bhambhani <sanjay.bhambani@litmus7.com>
2023-03-06 13:37:04 +0530

    inital commit

--- src/widget/widget.tsx
+++ src/widget/widget.tsx
@@ -1,12 +1,14 @@
 import React from "react"; // we need this to make JSX compile
 import { WidgetProps } from "./Interface";
 import "./style.css";
+import Header from "./Header";
 
 const Widget = ({
   title = "Widget Boilterplate",
   paragraph = "This is a Sample Widget",
 }: WidgetProps): JSX.Element | null => (
   <div className="widget-name-wrapper">
+    <Header />
     <div className="text-orange-600">
       <h2>{title}</h2>
     </div>
